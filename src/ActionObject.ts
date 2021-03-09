a1f35893614e4c6e4f2a2932f0ee38d81c9e521b
Louis Birla <louis.birla@gmail.com>
2021-03-08 18:25:09 -0600

    feat: add a redirect as an action object field

--- src/ActionObject.ts
+++ src/ActionObject.ts
@@ -9,4 +9,17 @@ export type ActionObject = {
 	/** A search component to open for the action.
 	    These components have their own action objects for after. */
 	search?: SearchComponent
+	/** Redirect to a link */
+	redirect?: RedirectObject
+}
+
+export type RedirectObject = {
+	/**
+	 * A path that goes after https://app.loop.page/
+	 * e.g `b/100` -> https://app.loop.page/b/100
+	 * This makes it easier for development.
+	 * */
+	app_path?: string
+	/** The full URL to redirect to */
+	url?: string
 }

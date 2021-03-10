99bb6d5abc5d5bc75e016bed23da778a7725786e
Louis Birla <louis.birla@gmail.com>
2021-03-10 17:37:15 -0600

    feat: add action popover

---
+++ src/components/actionpopover.ts
@@ -0,0 +1,20 @@
+import { IconName } from "./icon"
+import { ActionObject } from "../ActionObject"
+import { ColorSchemes } from "../ColorSchemes"
+import { ComponentObject } from "../ComponentObject"
+
+export type ActionPopoverComponent = {
+	cid: "actionpopover"
+	args: ActionPopoverArgs
+}
+
+export type ActionPopoverArgs = {
+	trigger?: ComponentObject
+	actions: ActionPopoverAction[]
+}
+
+export type ActionPopoverAction = {
+	icon?: IconName
+	text: string
+	interact?: ActionObject
+}
